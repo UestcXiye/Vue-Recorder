548f57ff8126cdf72155bdc02352b89b1d4884a1
兮夜 <812288728@qq.com>
2023-12-19 17:12:28 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 引入 element-ui
+import ElementUI from 'element-ui';
+// element-ui 的 css 样式要单独引入                    
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
